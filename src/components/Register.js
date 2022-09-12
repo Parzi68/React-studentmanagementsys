672a318b874608b5ebcfc3d79991592de9d4a66d
Smit Anam <78103851+Parzi68@users.noreply.github.com>
2022-09-12 13:42:22 +0530

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,27 +1,38 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+
 
 function BasicExample() {
   return (
     <Form>
+      <Container>
+      <Card>
+      <Card.Header>Register</Card.Header>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Card.Title><Form.Label>Username</Form.Label></Card.Title>
+        <Form.Control type="text" placeholder="Enter username" />
         <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
-
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter Email" />
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
+      </Card>
+      
       <Button variant="primary" type="submit">
         Submit
       </Button>
+      
+      </Container>
     </Form>
   );
 }
